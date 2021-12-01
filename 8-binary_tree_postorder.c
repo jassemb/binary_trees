f4264d7c7785654d6fd1db5b370cbf639e4b8e0a
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 <benalijassem201@gmail.com>
2021-12-01 05:57:15 -0800

    postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - function that
+ * binary_tree_postorder - function that
  * goes through a binary tree using postorder traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node.
